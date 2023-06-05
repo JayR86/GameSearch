78fb0903b40cdf038de9f5c96fff79a67b52f7f3
JAYR86 <jairoramirez719@gmail.com>
2023-06-05 14:53:44 -0700

    compress files' '

--- server/server.js
+++ server/server.js
@@ -28,7 +28,11 @@ app.use(express.json());
 
 // If we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  // app.use(express.static(path.join(__dirname, './client/build')));
+  app.use(express.static(path.resolve(__dirname,  "../client/build")));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "../client/build", "index.html"));
+    });
 }
 
 db.once('open', () => {
